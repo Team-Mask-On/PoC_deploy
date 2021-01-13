1de881c3c469c6b975bd6fcab82c2026c64ba9ca
HyunTaek5 <oht222@naver.com>
2021-01-14 01:29:58 +0900

    [upload] aws createsuperuser command 생성

---
+++ photos/management/commands/createsu.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        admin = User.objects.get_or_none(username="admin")
+        if not admin:
+            User.objects.create_superuser("admin", "admin@admin.com", "1234")
+            self.stdout.write(self.style.SUCCESS(f"Superuser Created"))
+        else:
+            self.stdout.write(self.style.SUCCESS(f"Superuser Exists"))
